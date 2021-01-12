8ddc20e2f246d2d4c0dd631bb507a6cc3d9dbfaa
Isaac Funes <ifunes@software.private>
2021-01-12 12:44:42 -0500

    Added 99 Bottles of Pop!

---
+++ script.js
@@ -0,0 +1,20 @@
+var word = "bottles";
+		var count = 99;
+		while(count > 0) {
+		    console.log(count + " " + word + " of pop on the wall");
+			document.write(count + " " + word + " of pop on the wall.");
+			document.write("<br>");
+			console.log(count + " " + word + " of pop, ");
+			document.write(count + " " + word + " of pop, ");
+			document.write("<br>");
+			console.log("Take one down, pass it around, ");
+			document.write("Take one down, pass it around, ");
+			document.write("<br>");
+			count = count - 1;
+			
+			if (count == 0) {
+				console.log("No more " + word + " of pop on the wall.");
+				document.write("No more " + word + " of pop on the wall.");
+			}
+		}
+		
