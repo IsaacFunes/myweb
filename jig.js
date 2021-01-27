1cec4826aff11b1e989971e1932b4febfa6e97ef
Isaac Funes <ifunes@software.private>
2021-01-27 14:07:05 -0500

    Jig Code Completed

--- jig.js
+++ jig.js
@@ -1,8 +1,10 @@
 var clunkCounter = 0;
 var x = 7;
-// The clunk function is pretty simple. it takes the argument passed down from the other function, facky, and
-// uses it as a counter. This counter is used to display clunk as many times as was defined when facky was created
-// in the last function.
+ 
+/*
+This Function is used for the variable that was passed to the thingamajig, this acts a counter which is used to display the string clunk
+as many times as we defined it.
+*/
 function clunk(times) {
 	var num = times;
 	while (num > 0) {
@@ -10,13 +12,16 @@ function clunk(times) {
 		num = num - 1
 	}
 }
-
-// The thingAMaJig works like this. First, it defines two variables in the local area, facky, and clunkCounter. 
-// Then, it makes a decision with an if, else if, and else statement. If size is zero, then it just says clank once.
-// Same for if the size equals one, only it says thunk once. When above one, things get tricky. It uses facky, the 
-// variable described in the beginning of the function, by multiplying it with the argument we passed in, and then
-// subtracting our argument by one. After, it puts the facky variable through the clunk function. 
-
+ 
+/*
+The actual mechinations of the thingamajig are hard to clarify. The clunkCounter resets to 0 every time thingamajig is 
+called because display (which thingamajig calls) modifies the counter. The size of the argument passed within the function
+is either a display of thunk or clunk, the decision statement will make this call. If thunk is called from the size being 1, then
+the variable facky is called which will be displayed on the console. However, if the size is over one then the facky variable 
+will be multiplied by the argument passed and then will subtract one from the argument. Clunk will be called once again with the facky's
+variable output. Initially all its doing is taking our passed variable and initalizing it to a new one and it will make a decision, and based
+on that decision, a display will be sent to the console or the web page.
+*/
 function thingamajig(size) {
 	var facky = 1;
 	clunkCounter = 0;
@@ -38,8 +43,8 @@ function thingamajig(size) {
 		clunk(facky);
 	}
 }
-// This is used to display the amount of times the thingAMaJig clunked.
 
+//This is the passed display that will be sent to the output.
 function display(output) {
 	console.log(output);
 	document.write(output);
@@ -47,6 +52,8 @@ function display(output) {
 	clunkCounter = clunkCounter + 1;
 }
 
+/* This loop calls the thingamajig and uses the defined varaibale (x) and will pass the number of times we assigned it (7) and will 
+display the clunkCounter, the variable defined above. */
 while (x >=0) {
 	thingamajig(x);
 	x -= 1;
@@ -54,5 +61,3 @@ while (x >=0) {
 	document.write(clunkCounter);
 	document.write("<br>");
 }
-
-
