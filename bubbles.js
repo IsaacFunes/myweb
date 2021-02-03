56488b9a76afb599ee0dd3b99ab6e7826a07fbc9
Isaac Funes <ifunes@software.private>
2021-02-03 13:28:16 -0500

    Added "Phrase-O-Matic"

--- bubbles.js
+++ bubbles.js
@@ -39,3 +39,22 @@ var scores = [60, 50, 60, 58, 54, 54, 58, 50, 52, 54, 48, 69,
  console.log("Solutions with the highest score: " + bestSolutions);
  document.write("Solutions with the highest score: " + bestSolutions);
  document.write("<br>");
+ 
+ function getMostCostEffectiveSolution(scores, costs, highscore) {
+	var cost = 100;
+	var index;
+	
+	for (var i = 0; i < scores.length; i++) {
+		if (scores[i] == highscore) {
+			if (cost > costs[i]) {
+				index = i;
+				cost = costs[i];
+			}
+		}
+	}
+    return index;
+}
+var mostCostEffective = getMostCostEffectiveSolution(scores, costs, highScore);
+console.log("Bubble Solution #" + mostCostEffective + " is the most cost effective");
+document.write("Bubble Solution #" + mostCostEffective + " is the most cost effective");
+document.write("<br>");
